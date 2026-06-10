6e45b9ae88fa3a793bf290784c0b48bc86b526c3
Hasina <hasina.nadaf@itvedant.com>
2026-06-10 23:40:45 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+       <button className='btn btn-primary'>Login</button>
+       <button className='btn btn-success'>Home</button>
+       <button className='btn btn-secondary'>About us</button>
+       <button className='btn btn-info'>Contact Us</button>
+    </div>
+  )
+}
+
+export default App;
